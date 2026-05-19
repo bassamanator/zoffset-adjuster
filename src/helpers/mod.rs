c940b81ea9e745aec18351f99b128568e8a628c3
Bassam Husain <bassam.husain@gmail.com>
2026-05-18 22:07:15 -0400

    cleanup

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,19 +1,14 @@
 // use serde::{Deserialize, Serialize};
-use std::{error::Error, fs, io, path};
+use std::{fs, io, path};
 
-pub const GCODE_DIR: &str = "./logs";
+pub const GCODE_DIR: &str = "./gcode";
 pub const GCODE_EXT: &str = "gcode";
-
-pub fn get_logs() -> Result<Vec<path::PathBuf>, io::Error> {
-    // if path::
-    // fs::create_dir_all(GCODE_DIR)?; // Ensure the directory exists
-
-    let log_files = fs::read_dir(GCODE_DIR)?
+pub fn get_gcode_files() -> Result<Vec<path::PathBuf>, io::Error> {
+    let gcode_files = fs::read_dir(GCODE_DIR)?
         .filter_map(|result| {
             // Map over directory entries, returning None if there's an error
             result.ok().and_then(|e| {
                 let path = e.path();
-                // Only include files, files with a .gcode extension
                 if path.is_file()
                     && path.extension().and_then(|ext| ext.to_str()) == Some(GCODE_EXT)
                 {
@@ -24,5 +19,5 @@ pub fn get_logs() -> Result<Vec<path::PathBuf>, io::Error> {
             })
         })
         .collect::<Vec<_>>();
-    Ok(log_files)
+    Ok(gcode_files)
 }
--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,13 @@ use inquire::{
 
 //     Ok(())
 // }
-pub const GCODE_DIR: &str = "./gcode";
-pub const GCODE_EXT: &str = "gcode";
 
 fn main() {
-    if let Err(e) = fs::create_dir_all(path::Path::new(GCODE_DIR)) {
-        eprintln!("Error creating directory '{}': {}", GCODE_DIR, e);
+    if let Err(e) = fs::create_dir_all(path::Path::new(helpers::GCODE_DIR)) {
+        eprintln!("Error creating directory '{}': {}", helpers::GCODE_DIR, e);
     }
-    let gcodes_list: Vec<path::PathBuf> = get_gcode_files().expect("Failed to get gcode list");
+    let gcodes_list: Vec<path::PathBuf> =
+        helpers::get_gcode_files().expect("Failed to get gcode list");
     let gcodes_list: Vec<String> = gcodes_list
         .iter()
         .map(|p| p.display().to_string())
@@ -41,23 +40,4 @@ fn main() {
         Ok(file) => println!("Selected: {:?}", file),
         Err(_) => println!("There was an error or the user canceled"),
     }
-}
-
-fn get_gcode_files() -> Result<Vec<path::PathBuf>, io::Error> {
-    let gcode_files = fs::read_dir(GCODE_DIR)?
-        .filter_map(|result| {
-            // Map over directory entries, returning None if there's an error
-            result.ok().and_then(|e| {
-                let path = e.path();
-                if path.is_file()
-                    && path.extension().and_then(|ext| ext.to_str()) == Some(GCODE_EXT)
-                {
-                    Some(path)
-                } else {
-                    None
-                }
-            })
-        })
-        .collect::<Vec<_>>();
-    Ok(gcode_files)
 }
